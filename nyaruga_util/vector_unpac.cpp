9c61bb0395aaab204063c94c5874e27ead3d1852
alphya <merukun1125@gmail.com>
2018-10-26 16:08:16 +0900

    update vector_unwrap

--- nyaruga_util/vector_unpac.cpp
+++ nyaruga_util/vector_unpac.cpp
@@ -4,43 +4,43 @@
 #include <vector>
 #include <iostream>
 
-template<std::size_t sentinel, std::size_t count, typename F, typename T, typename Alloc>
-std::function<void()> runtime_unpack_impl(F&& f, const std::vector<T, Alloc>& v)
+template<std::size_t sentinel, std::size_t count, typename R, typename F, typename Itr, typename Itr2>
+std::function<R()> runtime_unpack_impl(F&& f, Itr && current_itr, Itr2 && end_itr)
 {
-    if(v.empty())
-        return std::function<void()>([f = std::forward<F>(f)](){return f();});
+    if(current_itr == end_itr)
+        return [f = std::forward<F>(f)](){return f();};
 
     if constexpr(sentinel >= count)
     {
-        auto cdr = v;
-        cdr.erase(cdr.begin());
-        auto f_ = [car = v.front(), f = std::forward<F>(f)](auto&& ... args)
+        auto f_ = [car = current_itr, f = std::forward<F>(f)](auto&& ... args)
         {
-            f(car, std::forward<decltype(args)>(args)...);
+            return f(*car, std::forward<decltype(args)>(args)...);
         };
-        return runtime_unpack_impl<sentinel, count+1>(f_, cdr);
+        return runtime_unpack_impl<sentinel, count+1, R>
+            (f_, std::forward<decltype(current_itr)>(++current_itr), 
+             std::forward<decltype(end_itr)>(end_itr));
     }
     else
     {
         std::cout << u8"引数が多すぎます。 " << sentinel << u8"個までの引数にしてください。\n";
-        return std::function<void()>([f = std::forward<F>(f)](){return f();});
+        return [f = std::forward<F>(f)](){return f();};
     }
 }
 
-template<std::size_t sentinel, typename F, typename T, typename Alloc>
-std::function<void()> runtime_unpack(F&& f, const std::vector<T, Alloc>& v)
+template<std::size_t sentinel, typename F, typename T>
+auto runtime_unpack(F&& func, const T& container) -> std::function<decltype(func(*std::begin(container)))()>
 {
-    return runtime_unpack_impl<sentinel, 1>(std::forward<F>(f), v);
+    return runtime_unpack_impl<sentinel, 1, decltype(func(*std::begin(container)))>(std::forward<F>(func), std::begin(container), std::end(container));
 }
 
 int main()
 {
     std::vector<int> v{10, 20, 30};
 
-    const auto lambda = [](auto&& ... args)
+    const auto lambda = [](auto && ... args)
     {
-        (std::cout << ... << std::forward<decltype(args)>(args)) << '\n';
+        return (0 + ... + std::forward<decltype(args)>(args));
     };
 
-    runtime_unpack<10>(lambda, v)();
+    std::cout << runtime_unpack<10>(lambda, v)();
 }
