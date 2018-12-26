3e2602cbd6e7706ea11fe558c69d79458133bbc1
sokamu_kuruga <akamu1125@hotmail.co.jp>
2018-12-26 21:28:25 +0900

    nyannyan

--- nyaruga_util/make_compile_error.hpp
+++ nyaruga_util/make_compile_error.hpp
@@ -7,22 +7,23 @@
 #ifndef NYARUGA_UTIL_MAKE_COMPILE_ERROR_HPP
 #define NYARUGA_UTIL_MAKE_COMPILE_ERROR_HPP
 
-#include <boost/static_assert.hpp>
-#include <boost/preprocessor/stringize.hpp>
-
 // MS compatible compilers support #pragma once
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
 # pragma once
 #endif
 
-#define NYARUGA_MAKE_COMPILE_ERROR( ... ) \
-static_assert(std::bool_constant<false>::value, \
-"\n\n\nNYARUGA_UTIL MAKED COMPILE ERROR!\n" \
+#include <boost/preprocessor/stringize.hpp>
+
+#define MAKE_COMPILE_ERROR( ... ) \
+static_assert([](auto && a) { \
+return std::remove_reference<decltype(a)>::type::value; \
+} (std::is_void<int>{}), \
+"\n\n\nMAKED COMPILE ERROR!\n" \
 "file: " BOOST_PP_STRINGIZE(__FILE__) "\n" \
 "line: " BOOST_PP_STRINGIZE(__LINE__) "\n" \
-"message: " BOOST_PP_STRINGIZE(__VA_ARGS__) "\n\n" \
-);
+"message: " BOOST_PP_STRINGIZE(#__VA_ARGS__) "\n\n" \
+)
 
 #endif // #ifndef NYARUGA_UTIL_MAKE_COMPILE_ERROR_HPP
 
