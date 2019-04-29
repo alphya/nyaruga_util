b8fc50f1b8c04192eb7c526e777ad9fe9feb6df5
alphya <merukun1125@gmail.com>
2019-04-29 23:57:07 +0900

    nyanya

---
+++ nyaruga_util/const_char_2_char.hpp
@@ -0,0 +1,39 @@
+
+//              Copyright (c) 2018 alphya
+// Distributed under the Boost Software License, Version 1.0.
+//   (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef NYARUGA_UTIL_CONST_CHAR_2_CHAR_HPP
+#define NYARUGA_UTIL_CONST_CHAR_2_CHAR_HPP
+
+#pragma once
+
+namespace nyaruga {
+
+namespace util {
+
+/* for copy and paste
+
+#include <vector>
+#include <cstdio> // snprintf
+
+std::string to = "some text";
+std::vector<char> str(to.size());
+std::snprintf(str.data(), str.size(), "%s", to.c_str());
+char * to = str.data();
+
+or
+
+const char from[] = "some text";
+std::vector<char> str(sizeof from);
+std::snprintf(str.data(), str.size(), "%s", from);
+char * to = str.data();
+
+*/
+
+} // namespace util
+
+} // namespace nyaruga
+
+#endif // #ifndef NYARUGA_UTIL_CONST_CHAR_2_CHAR_HPP
