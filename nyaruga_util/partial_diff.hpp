0a12007f1cc70def179e1b060ed58ea458f5b227
alphya <merukun1125@gmail.com>
2019-10-23 00:23:11 +0900

    uptate

--- nyaruga_util/partial_diff.hpp
+++ nyaruga_util/partial_diff.hpp
@@ -19,7 +19,7 @@ namespace nyaruga::util {
 
 namespace hana = boost::hana;
 
-// •Î”÷•ª‚µ‚Ü‚·
+// Partial differentiation
 template <std::size_t count, typename NumType = num_t, typename F>
 constexpr auto partial_diff(F && f) noexcept
 {
@@ -31,5 +31,17 @@ constexpr auto partial_diff(F && f) noexcept
 }
 
 } // namespace nyaruga::util
+
+/* usage
+#include <iostream>
+#include <nyaruga_util/partial_diff.hpp>
+
+int main()
+{
+   auto lambda = [](auto ... a) { return static_cast<nyaruga::util::num_t>((a * ... )); };
+
+   std::cout << std::setprecision(18) << nyaruga::util::partial_diff<1>(lambda)(nyaruga::util::num_t(2), 5., 6.);
+}
+*/
 
 #endif // #ifndef NYARUGA_UTIL_PARTIAL_DIFF_HPP
