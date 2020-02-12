f01b2ea606c035046ed5bcc44e19b6dcef6ad638
alphya <merukun1125@gmail.com>
2020-02-12 15:27:39 +0900

    update

--- test/make_compile_error/test.cpp
+++ test/make_compile_error/test.cpp
@@ -2,13 +2,12 @@
 #define BOOST_TEST_MODULE make_compile_error_test
 #include <boost/test/included/unit_test.hpp>
 
-#include "../../nyaruga_util/pac_select_pos_obj.hpp"
 #include <string>
 
 BOOST_AUTO_TEST_CASE(make_compile_error_test_1)
 {
    [[maybe_unused]] auto ll = [](auto &&... hh) {
-      return nyaruga::util::pac_select_pos_obj<0>(hh...);
+      return 0;
    };
 
    // BOOST_CHECK(ll(9, 10, 11) == 10);
