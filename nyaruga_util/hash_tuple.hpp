cc2bf63e3726b58e2688814078a5b818b9d0da4e
alphya <merukun1125@gmail.com>
2020-09-04 19:50:12 +0900

    add hash_tuple.hpp

---
+++ nyaruga_util/hash_tuple.hpp
@@ -0,0 +1,71 @@
+
+//              Copyright (c) 2018 alphya
+// Distributed under the Boost Software License, Version 1.0.
+//   (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef NYARUGA_UTIL_HASH_TUPLE_HPP
+#define NYARUGA_UTIL_HASH_TUPLE_HPP
+
+#pragma once
+
+#include <tuple>
+
+// std::unordered_map などを使う際、key に std::tuple を使用できるようにする
+
+// 使い方：このファイルをインクルードする
+
+
+// Code from https://stackoverflow.com/questions/20834838/using-tuple-in-unordered-map
+namespace std{
+    namespace
+    {
+
+        // Code from boost
+        // Reciprocal of the golden ratio helps spread entropy
+        //     and handles duplicates.
+        // See Mike Seymour in magic-numbers-in-boosthash-combine:
+        //     http://stackoverflow.com/questions/4948780
+
+        template <class T>
+        inline void hash_combine(std::size_t& seed, T const& v)
+        {
+            seed ^= std::hash<T>()(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+        }
+
+        // Recursive template code derived from Matthieu M.
+        template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
+        struct HashValueImpl
+        {
+          static void apply(size_t& seed, Tuple const& Tuple)
+          {
+            HashValueImpl<Tuple, Index-1>::apply(seed, Tuple);
+            hash_combine(seed, std::get<Index>(Tuple));
+          }
+        };
+
+        template <class Tuple>
+        struct HashValueImpl<Tuple,0>
+        {
+          static void apply(size_t& seed, Tuple const& Tuple)
+          {
+            hash_combine(seed, std::get<0>(Tuple));
+          }
+        };
+    }
+
+    template <typename ... TT>
+    struct hash<std::tuple<TT...>> 
+    {
+        size_t
+        operator()(std::tuple<TT...> const& tt) const
+        {                                              
+            size_t seed = 0;                             
+            HashValueImpl<std::tuple<TT...> >::apply(seed, tt);    
+            return seed;                                 
+        }                                              
+
+    };
+}
+
+#endif // NYARUGA_UTIL_HASH_TUPLE_HPP
