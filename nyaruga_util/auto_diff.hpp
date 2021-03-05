f1c0d59ec519cc2154ae51d2a6f8484b31f1b532
alphya <merukun1125@gmail.com>
2021-03-05 22:52:14 +0900

    update

--- nyaruga_util/auto_diff.hpp
+++ nyaruga_util/auto_diff.hpp
@@ -1,7 +1,7 @@
 
 /*!
  * @file 
- * @brief Defines configuration macros used throughout the library.
+ * @brief 
  * @copyright alphya 2021
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
--- nyaruga_util/private_accessor.hpp
+++ nyaruga_util/private_accessor.hpp
@@ -1,7 +1,7 @@
 ﻿
 /*!
  * @file 
- * @brief Defines configuration macros used throughout the library.
+ * @brief 
  * @copyright alphya 2018-2021
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
